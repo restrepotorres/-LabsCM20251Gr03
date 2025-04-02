bd9b043dfab2689cd543acb59f6df51e2884b611
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2025-04-01 21:19:00 -0500

    punto 4 finalizado

--- app/src/main/java/com/co/labscm20251_gr03/PersonalDataActivity.kt
+++ app/src/main/java/com/co/labscm20251_gr03/PersonalDataActivity.kt
@@ -1,17 +1,26 @@
 package com.co.labscm20251_gr03
 
+import android.app.DatePickerDialog
+import android.icu.text.DisplayOptions.Capitalization
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -22,11 +31,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.co.labscm20251_gr03.ui.theme.LabsCM20251Gr03Theme
 import java.time.LocalDate
+import java.util.Calendar
 
 class PersonalDataActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +49,9 @@ class PersonalDataActivity : ComponentActivity() {
         setContent {
             Formulario()
 
-            }
         }
     }
+}
 
 
 @Composable
@@ -54,8 +68,22 @@ fun Formulario() {
     var nombre by remember { mutableStateOf(TextFieldValue()) }
     var apellidos by remember { mutableStateOf(TextFieldValue()) }
     var sexo by remember { mutableStateOf("") }
-    var fechaNacimiento: LocalDate
-    var gradoEscolaridad = listOf<String>("Primaria", "Secundaria", "Universitaria", "Otro")
+    var fechaNacimiento by remember { mutableStateOf("Seleccionar fecha") }
+    var escolaridad by remember {mutableStateOf("")}
+    val opcionesEscolaridad = listOf("Primaria", "Secundaria", "Universitaria", "Otro")
+    var expanded by remember { mutableStateOf(false) }
+
+
+
+    val datePickerDialog = DatePickerDialog(
+        LocalContext.current,
+        { _, year, month, dayOfMonth ->
+            fechaNacimiento = "$dayOfMonth/${month + 1}/$year"
+        },
+        Calendar.getInstance().get(Calendar.YEAR),
+        Calendar.getInstance().get(Calendar.MONTH),
+        Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+    )
 
     Column(
         modifier = Modifier.padding(16.dp),
@@ -66,33 +94,76 @@ fun Formulario() {
         OutlinedTextField(
             value = nombre,
             onValueChange = { nombre = it },
-            label = { Text("Nombres*") }
+            label = { Text("Nombres*") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            )
         )
         OutlinedTextField(
             value = apellidos,
             onValueChange = { apellidos = it },
-            label = { Text("Apellidos*") }
+            label = { Text("Apellidos*") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            )
         )
 
         Text(text = "Sexo*")
-        Row{
+        Row {
             Text("Masculino")
-            Spacer(modifier = Modifier.width(16.dp))
             RadioButton(
                 selected = sexo == "Masculino",
-                onClick = {sexo = "Masculino"}
+                onClick = { sexo = "Masculino" }
             )
-
             Text("Femenino")
-            Spacer(modifier = Modifier.width(16.dp))
             RadioButton(
                 selected = (sexo == "Femenino"),
-                onClick = {sexo = "Femenino"}
+                onClick = { sexo = "Femenino" }
+            )
+            Text("Otro")
+            RadioButton(
+                selected = (sexo == "Otro"),
+                onClick = { sexo = "Otro" }
             )
 
         }
-        Button(onClick = { println("la buena") }) {
-            Text("Enviar")
+
+        Text(text = "Fecha de Nacimiento*")
+        Button(onClick = { datePickerDialog.show() }) {
+            Text(fechaNacimiento)
+        }
+
+
+
+        var expanded by remember { mutableStateOf(false) }
+        Box {
+            Button(onClick = { expanded = true }) {
+                Text( if (escolaridad == "") "Escolaridad" else escolaridad, style = MaterialTheme.typography.bodyLarge)
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                opcionesEscolaridad.forEach { opcion ->
+                    DropdownMenuItem(
+                        text = { Text(opcion) },
+                        onClick = {
+                            escolaridad = opcion
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Button(onClick = { println("la buena, pero falta validar los campos obligatorios") }) {
+            Text("Enviar") //Todo: validar los campos obligatorios
         }
 
     }
