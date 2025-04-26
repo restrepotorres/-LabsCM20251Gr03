7846c18789abfbf2fbebf5bb663b064a89ba50f5
Rodas <68786750+Rodas033@users.noreply.github.com>
2025-04-26 00:25:23 -0500

    Colocar iconos y estructurar visualmente

--- app/src/main/java/com/co/labscm20251_gr03/ui/element/Form.kt
+++ app/src/main/java/com/co/labscm20251_gr03/ui/element/Form.kt
@@ -1,5 +1,6 @@
 package com.co.labscm20251_gr03.ui.element
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -25,24 +26,26 @@ fun CampoPais(
 
     var sugerenciasPais by remember { mutableStateOf(listOf<String>()) }
 
-    OutlinedTextField(
-        value = pais,
-        onValueChange = {
-            onPaisChange(it)
-            obtenerPaises(context, it) { suggestions -> sugerenciasPais = suggestions }
-        },
-        label = { Text("País*") },
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.Words,
-            autoCorrect = false,
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Next
+    Column {
+        OutlinedTextField(
+            value = pais,
+            onValueChange = {
+                onPaisChange(it)
+                obtenerPaises(context, it) { suggestions -> sugerenciasPais = suggestions }
+            },
+            label = { Text("País*") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            )
         )
-    )
 
-    sugerenciasPais.forEach {
-        TextButton(onClick = { onPaisChange(it); sugerenciasPais = emptyList() }) {
-            Text(it)
+        sugerenciasPais.forEach {
+            TextButton(onClick = { onPaisChange(it); sugerenciasPais = emptyList() }) {
+                Text(it)
+            }
         }
     }
 }
@@ -57,24 +60,26 @@ fun CampoCiudad(
 
     var sugerenciasCiudad by remember { mutableStateOf(listOf<String>()) }
 
-    OutlinedTextField(
-        value = ciudad,
-        onValueChange = {
-            onCiudadChange(it)
-            obtenerCiudades(context, pais, it) { suggestions -> sugerenciasCiudad = suggestions }
-        },
-        label = { Text("Ciudad") },
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.Words,
-            autoCorrect = false,
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Next
+    Column{
+        OutlinedTextField(
+            value = ciudad,
+            onValueChange = {
+                onCiudadChange(it)
+                obtenerCiudades(context, pais, it) { suggestions -> sugerenciasCiudad = suggestions }
+            },
+            label = { Text("Ciudad") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            )
         )
-    )
 
-    sugerenciasCiudad.forEach {
-        TextButton(onClick = { onCiudadChange(it); sugerenciasCiudad = emptyList() }) {
-            Text(it)
+        sugerenciasCiudad.forEach {
+            TextButton(onClick = { onCiudadChange(it); sugerenciasCiudad = emptyList() }) {
+                Text(it)
+            }
         }
     }
 }
